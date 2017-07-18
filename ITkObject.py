cb777f0974d9442418f2afe5ae4b4ab4f934c7b2
teh1337z <adamazoulay@live.ca>
2017-07-17 22:11:58 -0400

    Added the pad coordinates to the ITkObject class

--- ITkObject.py
+++ ITkObject.py
@@ -5,7 +5,7 @@
 
 class ITkObject(object):
 
-    def __init__(self,name,pads,wires,):
+    def __init__(self,name,pads,wires,padCoords):
         #Name of the hybrid (i.e. Barrel 130nm Hybrid)
         self.name = name
 
@@ -15,4 +15,5 @@ class ITkObject(object):
         #list with 3-tuples containing (pad name, pad description, and ASIC the wire comes from)
         self.wires = wires
 		
-		#
+		#List of (x,y) tuples containing the top left x,y coords for the bond pads
+		self.padCoords = padCoords
