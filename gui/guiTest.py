16943d37ca5bd468cb40cd14402b952f0afa0c9f
Adam A <adamazoulay@live.ca>
2017-07-31 13:39:09 -0400

    Updated ui tests

--- gui/guiTest.py
+++ gui/guiTest.py
@@ -1,4 +1,5 @@
 from PyQt4 import QtGui # Import the PyQt4 module we'll need
+from PyQt4 import QtCore # for quit button
 import sys # We need sys so that we can pass argv to QApplication
 
 
@@ -11,6 +12,23 @@ class MainApp(QtGui.QMainWindow, QAgui.Ui_Dialog):
         super(self.__class__, self).__init__()
         self.setupUi(self)  # This is defined in design.py file automatically
                             # It sets up layout and widgets that are defined
+
+        #If module is selected, populate hybrid selection
+        self.moduleName.currentIndexChanged.connect(self.populate_hybrids)
+
+        #quit
+        self.qButton.clicked.connect(QtCore.QCoreApplication.instance().quit)
+        
+    def populate_hybrids(self):
+        moduleNum = self.moduleName.currentIndex() - 1
+        self.hybridName.clear()
+
+        if moduleNum >= 0:
+            self.hybridName.addItem(str(moduleNum))
+        
+    
+
+    
 
 def displayGui():
     app = QtGui.QApplication(sys.argv)  # A new instance of QApplication
