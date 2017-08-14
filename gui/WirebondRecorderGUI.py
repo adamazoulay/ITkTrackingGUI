ace2b7778fb4650c6521a71e8d1c4e69fe8b2407
Adam A <adamazoulay@live.ca>
2017-08-14 12:37:09 -0400

    Added beta selection mode, changed style sheets

--- gui/WirebondRecorderGUI.py
+++ gui/WirebondRecorderGUI.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'WirebondRecorderGUI.ui'
 #
-# Created by: PyQt5 UI code generator 5.9
+# Created by: PyQt5 UI code generator 5.8.1
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -19,7 +19,8 @@ class Ui_WirebondRecorder(object):
         WirebondRecorder.setSizePolicy(sizePolicy)
         self.imgSelect = QtWidgets.QLabel(WirebondRecorder)
         self.imgSelect.setGeometry(QtCore.QRect(10, 120, 687, 660))
-        self.imgSelect.setAutoFillBackground(True)
+        self.imgSelect.setAutoFillBackground(False)
+        self.imgSelect.setStyleSheet("border: 2px solid black;")
         self.imgSelect.setFrameShape(QtWidgets.QFrame.Box)
         self.imgSelect.setFrameShadow(QtWidgets.QFrame.Plain)
         self.imgSelect.setText("")
@@ -53,15 +54,23 @@ class Ui_WirebondRecorder(object):
         self.horizontalLayout.setSizeConstraint(QtWidgets.QLayout.SetDefaultConstraint)
         self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
         self.horizontalLayout.setObjectName("horizontalLayout")
-        self.pushButton_5 = QtWidgets.QPushButton(self.horizontalLayoutWidget)
+        self.btnRun = QtWidgets.QPushButton(self.horizontalLayoutWidget)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Minimum)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.pushButton_5.sizePolicy().hasHeightForWidth())
-        self.pushButton_5.setSizePolicy(sizePolicy)
-        self.pushButton_5.setIconSize(QtCore.QSize(16, 16))
-        self.pushButton_5.setObjectName("pushButton_5")
-        self.horizontalLayout.addWidget(self.pushButton_5)
+        sizePolicy.setHeightForWidth(self.btnRun.sizePolicy().hasHeightForWidth())
+        self.btnRun.setSizePolicy(sizePolicy)
+        self.btnRun.setIconSize(QtCore.QSize(16, 16))
+        self.btnRun.setObjectName("btnRun")
+        self.horizontalLayout.addWidget(self.btnRun)
+        self.btnSave = QtWidgets.QPushButton(self.horizontalLayoutWidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Minimum)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.btnSave.sizePolicy().hasHeightForWidth())
+        self.btnSave.setSizePolicy(sizePolicy)
+        self.btnSave.setObjectName("btnSave")
+        self.horizontalLayout.addWidget(self.btnSave)
         spacerItem = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.horizontalLayout.addItem(spacerItem)
         self.qButton = QtWidgets.QPushButton(self.horizontalLayoutWidget)
@@ -82,7 +91,8 @@ class Ui_WirebondRecorder(object):
         self.label_3.setText(_translate("WirebondRecorder", "Current level:"))
         self.btnBack.setText(_translate("WirebondRecorder", "Back"))
         self.label.setText(_translate("WirebondRecorder", "Wirebond Recorder"))
-        self.pushButton_5.setText(_translate("WirebondRecorder", "Run"))
+        self.btnRun.setText(_translate("WirebondRecorder", "Run"))
+        self.btnSave.setText(_translate("WirebondRecorder", "Save"))
         self.qButton.setText(_translate("WirebondRecorder", "Exit"))
 
 
--- gui/guiTest.py
+++ gui/guiTest.py
@@ -13,16 +13,18 @@ from WirebondRecorderGUI import Ui_WirebondRecorder
 #Make image loading a function
 
 #================================================================================
-#Define the classes for the various guis
 
+#Define the classes for the various guis
 class WirebondRecorder(QtWidgets.QMainWindow, Ui_WirebondRecorder):
     def __init__(self):
         super(self.__class__, self).__init__()
         self.setupUi(self)  # This is defined in design.py file automatically
                             # It sets up layout and widgets that are defined
 
-        #Start an array here to keep track of the level we are at
+        #Start global variables
         self.level = ['root']
+        self.selectionMode = False
+        self.browseMode = True
 
         #Set level label (maybe debug?)
         self.levelLabel.setText(self.level[-1])
@@ -38,7 +40,7 @@ class WirebondRecorder(QtWidgets.QMainWindow, Ui_WirebondRecorder):
                          "R0H1" : [(88,354), (582,355), (575,460), (100,456)]}
         activeAreasR0H0 = {"ASIC" : [(21,282), (73,278), (26,319), (76,315)]}
         activeAreasR0H1 = {"ASIC" : [(76,221), (122,220), (80,252), (126,248)]}
-        activeAreasASIC = {"pad1" : []} #etc
+        activeAreasASIC = {"pad1" : [(0,0), (0,0), (0,0), (0,0)]} #etc
 
         self.activeAreas = {"root" : activeAreasRoot, "R0" : activeAreasR0,\
                             "R0H0" : activeAreasR0H0, "R0H1" : activeAreasR0H1,\
@@ -53,20 +55,29 @@ class WirebondRecorder(QtWidgets.QMainWindow, Ui_WirebondRecorder):
         #Back button
         self.btnBack.clicked.connect(self.levelUp)
 
+        #Run button
+        self.btnRun.clicked.connect(self.changeModeSelect)
+
         #hide form
         self.qButton.clicked.connect(self.hide)
 
     #Back button functionality
     def levelUp(self):
-        #Check we aren't at root
-        if self.level[-1] != "root":
+        #Check we aren't at root and are in browse mode
+        if self.level[-1] != "root" and self.browseMode:
             self.level.pop(-1)
             name = self.level[-1]
             self.levelLabel.setText(self.level[-1])
             print(name)
 
             self.imgSelect.setPixmap(QtGui.QPixmap('imgs/' + name + '.jpg',"1"))
-        
+
+        #Switch back to browse mode (warning screen?)
+        if self.selectionMode:
+            self.browseMode = True
+            self.selectionMode = False
+            self.imgSelect.setStyleSheet("border: 2px solid black;")
+            
 
     def executeSelection(self, ev):
         #Grab click location    
@@ -75,25 +86,35 @@ class WirebondRecorder(QtWidgets.QMainWindow, Ui_WirebondRecorder):
 
         print('(' + str(x) + ',' +  str(y) + '),') #DEBUG
 
-        #Check if it's inside any of the current active areas
-        curDict = self.activeAreas[self.level[-1]]
-        for area in curDict:
-            name = area
-            coords = curDict[name]
-            tempPath = mplPath.Path(np.array([coords[0], coords[1],\
-                                              coords[2], coords[3]]))
-            inside = tempPath.contains_point((x,y))
+        if self.browseMode:
+            #Check if it's inside any of the current active areas
+            curDict = self.activeAreas[self.level[-1]]
+            for area in curDict:
+                name = area
+                coords = curDict[name]
+                tempPath = mplPath.Path(np.array([coords[0], coords[1],\
+                                                  coords[2], coords[3]]))
+                inside = tempPath.contains_point((x,y))
 
-            #If it is, do stuff
-            if inside:
-                #Make this all a function for use with the back button!!
-                #print name #DEBUG
-                self.level.append(name) #Add level to level array
-                self.levelLabel.setText(self.level[-1]) #change level label DEBUG?
+                #If it is, do stuff
+                if inside:
+                    #Make this all a function for use with the back button!!
+                    #print name #DEBUG
+                    self.level.append(name) #Add level to level array
+                    self.levelLabel.setText(self.level[-1]) #change level label DEBUG?
 
-                #Need to place the new picture
-                print('imgs/' + name + '.jpg') #DEBUG
-                self.imgSelect.setPixmap(QtGui.QPixmap('imgs/' + name + '.jpg','1'))
+                    #Need to place the new picture
+                    print('imgs/' + name + '.jpg') #DEBUG
+                    self.imgSelect.setPixmap(QtGui.QPixmap('imgs/' + name + '.jpg','1'))
+
+        if self.selectionMode:
+            print("cool")
+
+
+    def changeModeSelect(self):
+        self.browseMode = False
+        self.selectionMode = True
+        self.imgSelect.setStyleSheet("border: 2px solid red;")
         
 
 class WelcomeWindow(QtWidgets.QMainWindow, Ui_WelcomeWindow):
